e059ac4e32049b89087b7c29dd8de01c4ded468e
Yao Min <ngyaominsg@gmail.com>
2020-08-28 13:52:55 +0800

    requestInsurance function

--- contracts/requestForInsurance.sol
+++ contracts/requestForInsurance.sol
@@ -7,3 +7,12 @@ contract ChainList {
   uint payout1;
   uint premium;
 }
+
+// request an insurance
+function requestInsurance(string _name, string _term1, uint _payout1, uint premium) publc {
+  seller = msg.sender;
+  name = _name;
+  term1 = _term1;
+  payout1 = _payout1;
+  premium = _premium;
+}
