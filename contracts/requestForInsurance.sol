83f2baa4da2f58c6f2c89074e426ff8bd0cb5bd9
Yao Min <ngyaominsg@gmail.com>
2020-08-28 14:05:09 +0800

    Some changes to variables naming, Do up getInsuranceRequest Fuction

--- contracts/requestForInsurance.sol
+++ contracts/requestForInsurance.sol
@@ -4,15 +4,27 @@ contract ChainList {
   address toBeInsured;
   string name;
   string term1;
-  uint payout1;
-  uint premium;
+  uint256 payout1;
+  uint256 premium;
 }
 
 // request an insurance
-function requestInsurance(string _name, string _term1, uint _payout1, uint premium) publc {
-  seller = msg.sender;
+function requestInsurance(string _name, string _term1, uint256 _payout1, uint256 premium) publc {
+  toBeInsured = msg.sender;
   name = _name;
   term1 = _term1;
   payout1 = _payout1;
   premium = _premium;
 }
+
+// get insurance request
+function getInsuranceRequest() public view returns (
+  address _toBeInsured,
+  string _name,
+  string _term1,
+  uint256 _payout1,
+  uint256 _premium
+  ) {
+    return(toBeInsured, name, term1, payout1, premium);
+  }
+  
