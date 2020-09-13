6de623e21dcf98340ea75338877948e6105a535b
Yao Min <ngyaominsg@gmail.com>
2020-09-13 17:33:30 +0800

    Init web3

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,15 @@ App = {
      contracts: {},
 
      init: function() {
-          /*
-           * Replace me...
-           */
-
-          return App.initWeb3();
+       return App.initWeb3();
      },
 
      initWeb3: function() {
-          /*
-           * Replace me...
-           */
+       if(typeof web3 !== 'undefined') {
+         App.web3Provider = web3.currentProvider;
+       } else {
+         App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+       }
 
           return App.initContract();
      },
--- test/InsuranceListTestPath.js
+++ test/InsuranceListTestPath.js
@@ -6,8 +6,16 @@ contract('InsuranceList', function(accounts) {
   var toBeInsured = accounts[1];
   var name = "Chio Bu";
   var term1 = "Boobs";
-  var payout1 = new BigNumber("1");
-  var premium1 = new BigNumber("0.1");
+  // var BigNumber = require('bignumber.js');
+  // var payout1 = new BigNumber("1");
+  // var premium1 = new BigNumber("0.1");
+  // var payout1 = BigInt(10);
+  // var premium1 = BigInt(1);
+  const BN = require('bn.js');
+  // var payout1 = new BN(10);
+  // var premium1 = new BN(1);
+  var payout1 = new BN(10);
+  var premium = new BN(1);
 
   it("Should initialize with empty values", function() {
     return InsuranceList.deployed().then(function(instance) {
@@ -24,15 +32,15 @@ contract('InsuranceList', function(accounts) {
   it("should request an insurance", function() {
     return InsuranceList.deployed().then(function(instance) {
       insuranceListInstance = instance
-      return insuranceListInstance.requestInsurance(name, term1, web3.utils.toWei(payout1, "ether"), web3.utils.toWei(premium1, "ether"), { from: toBeInsured});
+      return insuranceListInstance.requestInsurance(name, term1, web3.utils.toWei(payout1, "ether"), web3.utils.toWei(premium, "ether"), { from: toBeInsured});
     }).then(function() {
       return insuranceListInstance.getInsuranceRequest();
     }).then(function(data) {
       assert.equal(data[0], toBeInsured, "toBeInsured must be " + toBeInsured);
       assert.equal(data[1], name, "Name must be " + name);
       assert.equal(data[2], term1, "Terms must be " + term1);
-      assert.equal(data[3].toNumber(), web3.utils.toWei(payout1, "ether"), "Payout must be " + payout1);
-      assert.equal(data[4].toNumber(),web3.utils.toWei(premium1, "ether") , "Premium must be " + payout1);
+      assert.equal(BN(data[3]), web3.utils.toWei(payout1, "ether"), "Payout must be " + web3.utils.toWei(payout1));
+      assert.equal(BN(data[4]), web3.utils.toWei(premium, "ether") , "Premium must be " + web3.utils.toWei(premium));
     });
   });
 });
